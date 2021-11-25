1178a1434ae6de09394717935c78c60062aafe4f
paularah <p.arah@alustudent.com>
2021-11-25 16:54:26 +0200

    BST insertion

--- Trees/tree.py
+++ Trees/tree.py
@@ -1,7 +1,40 @@
 """
-Root ->
-Edge ->  
-Leaf -> 
+Root -> first node
+Edge ->  connection between the children 
+Leaf -> the end child 
 Parent -> 
 Child -> 
 """
+
+
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.right = None
+        self.left = None
+
+
+class BST:
+    def __init__(self) -> None:
+        self.root = None
+
+    def insert(self, value):
+        newNode = Node(value)
+        if self.root == None:
+            self.root = newNode
+            return self
+        current = self.root
+        while True:
+            if current.value == value:
+                return None
+            elif current.value < value:
+                if current.left == None:
+                    current.left == newNode
+                    return self
+                else:
+                    current = current.left
+            else:
+                if current.right == None:
+                    current.right = newNode
+                else:
+                    current = current.right
