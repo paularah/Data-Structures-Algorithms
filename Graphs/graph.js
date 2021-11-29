b3dde191c628fbbb07b00f496f4e292b99602dae
paularah <p.arah@alustudent.com>
2021-11-30 00:01:11 +0200

    basic graoh implemntstion

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -1,9 +1,32 @@
-class Grpah{
-    constructor(){
+class Grpah {
+    constructor() {
         this.adjacencyList = {}
     }
 
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex])this.adjacencyList[vertex] = []
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2)
+        this.adjacencyList[vertex2].push(vertex1)
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((vertex) => {
+            return vertex != vertex2
+        })
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((vertex) => {
+            return vertex != vertex1
+        })
+
+    }
+
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex, adjacentVertex)
+        }
+        delete this.adjacencyList[vertex]
     }
 }
