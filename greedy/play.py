c8dc0ee3f8e40edfab1eb5ce96c2e5bd87b02952
paularah <p.arah@alustudent.com>
2022-09-29 17:45:38 +0200

    two city

---
+++ greedy/play.py
@@ -0,0 +1,19 @@
+def twoCitySchedule(costs):
+
+    diffs = []
+
+    for costA, costB in costs:
+        diffs.append([costB-costA, costA, costB])
+    diffs.sort()
+    
+    minCost = 0
+    for i in range(len(diffs)):
+        if i < len(diffs)//2:
+            minCost += diffs[2]
+        else:
+            minCost += diffs[1]
+    
+    return minCost
+
+assert twoCitySchedule([[10,20],[30,200],[400,50],[30,20]]) == 110
+        
