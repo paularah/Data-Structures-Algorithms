7daedc331c58ff116267df61f7adfc03dc42a178
paularah <p.arah@alustudent.com>
2022-10-02 06:11:32 +0200

    stack egs

--- stacks-queues/play.py
+++ stacks-queues/play.py
@@ -22,3 +22,38 @@ def candyCrush(string):
 assert candyCrush("aaabbbc") == "c"
 
 
+#https://leetcode.com/problems/decode-string/description/
+"""
+* create a stack 
+* loop through every character in the stack 
+* if the character is not a closing bracket, add it to the stack 
+* if the char is a closing bracket, pop everything for the stack until you find 
+the last correspondinfg opening bracket. 
+* Pop everything from the stack that is a digit to get the number 
+* create the substring n time and add it to the stack  
+"""
+def decodeString(s):
+    stack = []
+    for c in  s:
+        if stack and c == ']':
+            subStr = ''
+            while stack[-1] != '[':
+                subStr = stack.pop() + subStr
+            stack.pop()
+            num = ""
+            while stack and stack[-1].isdigit():
+                num = stack.pop() + num 
+            print(num)
+            stack.append(subStr * int(num))
+        else:
+            stack.append(c)
+    return "".join(stack)
+
+
+assert decodeString("3[a]2[bc]") == "aaabcbc"
+
+
+
+
+
+
