6d3c10b5d7d4e9c047ab7f89b5ec378b8c578413
paularah <p.arah@alustudent.com>
2022-10-21 15:37:40 +0200

    ignore swap files

--- hash-tables/main.go
+++ hash-tables/main.go
@@ -1,19 +1,63 @@
 package main
 
-type IhashTable interface {
-	insert(key string)
-	remove(key string)
-	get(key string) string
+import (
+	"errors"
+)
+
+const ArraySize int = 9
+
+// type IhashTable interface {
+// 	insert(key string)(error)
+// 	remove(key string)(error)
+// 	get(key string) (string, error)
+// }
+
+// type IlinkedList interface {
+// 	insert(val string)
+// 	remove(val string)
+// 	search(val string)
+// }
+
+
+type hashTable struct {
+	array [ArraySize]*bucket
 }
 
-type IlinkedList interface {
-	insert(val string)
-	remove(val string)
-	search(val string)
+type node struct {
+	value string
+	key string
+	next  *node
 }
 
-func InitHashTable(size int) *IhashTable {
-	var array [size]*bucket
+
+type bucket struct {
+	head *node
+}
+
+func (h *hashTable) get (key string) (string, error) {
+	keyHash := hash(key, ArraySize)
+	result, err := h.array[keyHash].get(key)
+	return result, err
+}
+
+func (h *hashTable) insert (key string, value string) {
+	keyHash := hash(key, ArraySize)
+	h.array[keyHash].insert(key, value)
+}
+
+func (h *hashTable) remove (key string) (err error){
+	keyHash := hash(key, ArraySize)
+	h.array[keyHash].remove(key)
+	return nil 
+}
+
+
+func InitHashTable() *hashTable {
+	var result = &hashTable{}
+	for i := range result.array {
+		result.array[i] = &bucket{}
+	}
+	return result
 }
 
 func hash(key string, size int) int {
@@ -25,36 +69,40 @@ func hash(key string, size int) int {
 }
 
 
-type hashTable struct {
-	array []*Bucket
+func (n *bucket) insert (key string, val string) {
+	currentNode := n.head 
+	for currentNode.next != nil {
+		currentNode = currentNode.next
+	}
+	currentNode.next = &node{value: val, key:key}
 }
 
-type bucket struct {
-	head *node
+func (n *bucket) remove (key string)  {
+	currentNode := n.head
+	var prevNode *node 
+
+	for currentNode.key != key {
+		prevNode = currentNode
+		currentNode = currentNode.next
+	}
+	prevNode.next = currentNode.next
+
 }
 
-func insert(node *bucket) (val string) {
-	temp := node.head 
-	for temp.next != nil {
-		temp = temp.next
+func (n *bucket) get(key string)(value string, err error) {
+	currentNode := n.head 
+	for currentNode.key != key && currentNode.next != nil {
+		currentNode = currentNode.next
 	}
 
-	temp.next = &node{val}
+	if currentNode.key != key {
+		return "", errors.New("non existent value")
+	}
+	return currentNode.value, nil 
+
 }
 
-func remove(node *bucket) (val string) {
-	temp := node.head
-	prev := nil 
-	for temp
-}
-
-
-
-type node struct {
-	value string
-	next  *bucket
-}
 
 func main() {
-
+	
 }
